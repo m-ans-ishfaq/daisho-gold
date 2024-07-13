7867ea7fcc9ab98e383640ade5603817d62feecf
Ans <anasashfaq00@gmail.com>
2024-07-14 01:19:58 +0500

    fixed build error

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -3,12 +3,12 @@ import NewsletterBg from "@/public/images/bike-parts-bg.png";
 import AuthBg from "@/public/images/bike-parts-bg-2.png";
 import Banner1 from "@/public/images/banner-1.jpg";
 import Banner2 from "@/public/images/banner-2.jpg";
-import Part1 from "@/public/parts/part1.JPG";
-import Part2 from "@/public/parts/part2.JPG";
 import CD70 from "@/public/bikes/cd-70.png";
 import CG125 from "@/public/bikes/cg-125.png";
 import GS150 from "@/public/bikes/gs-150.png";
 import HondaDelux from "@/public/bikes/honda-delux.png";
+const Part1 = require("@/public/parts/part1.JPG");
+const Part2 = require("@/public/parts/part2.JPG");
 
 export {
     DaishoGoldLogo,
